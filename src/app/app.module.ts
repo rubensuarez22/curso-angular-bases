68ba1e19e6e5e50fcb47b91bfb3c96eec5ae4d89
Ruben Suarez <ruben.suarezdz@udlap.mx>
2024-09-11 13:55:34 -0600

    seccion 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { CounterModule } from './counter/conter.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesModule} from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 
 @NgModule({
@@ -17,6 +18,7 @@ import { HeroesModule} from './heroes/heroes.module';
     AppRoutingModule,
     CounterModule,
     HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
